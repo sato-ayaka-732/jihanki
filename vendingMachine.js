d401576e6ea7813f0837b73819c846bdabf0d8d3
sato-ayaka-732 <100703ayaka@gmail.com>
2025-11-11 15:09:55 +0900

    画像を横並びに配置する

--- vendingMachine.js
+++ vendingMachine.js
@@ -1,3 +1,12 @@
 // ここからJavaScriptを書いていきます
 // まずは「表示する」「押すと何か起きる」などを学べるようにしていきます
 console.log("自動販売機ページを読み込みました！");
+
+const drinks = document.querySelectorAll(".drink");
+
+// 簡単なforループで、飲み物が押されたときにメッセージを表示するようにします
+for (let i = 0; i < drinks.length; i++) {
+    drinks[i].addEventListener("click", function() {
+        console.log(drinks[i].alt + " がクリックされました！");
+    });
+}
